9aeb8c575951bc9a263aa0f608decca4a4946e95
Bram Howard <bram@thevoid.com>
2017-07-24 17:33:47 -0500

    Progress on making the balls.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+class PlayerCamera : MonoBehaviour
+{
+	public GameObject Player;
+
+	private Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - Player.transform.position;
+	}
+
+	void LateUpdate()
+	{
+		transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/Propellor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+class Propellor : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float thrust = 0;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddForce(transform.up * thrust);
+	}
+}
