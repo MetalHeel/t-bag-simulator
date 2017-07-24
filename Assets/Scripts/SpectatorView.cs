9e0c6387a567b66d1958d7e343618b64245c9403
Bram Howard <bram@thevoid.com>
2017-07-24 14:54:00 -0500

    Add some balls.

--- Assets/Scripts/SpectatorView.cs
+++ Assets/Scripts/SpectatorView.cs
@@ -4,41 +4,38 @@
 public class SpectatorView : MonoBehaviour
 {
 	private const float moveSpeed = 0.1f;
+	private const float lookSpeed = 2.0f;
 
-	private Vector3 startMousePos = Vector3.zero;
+	private float yaw = 0.0f;
+	private float pitch = 0.0f;
 
 	void Update()
 	{
-		Vector3 pos = transform.position;
-
-		// Forward/Back.
+		// Forward/Back
 		if (Input.GetKey(KeyCode.W))
-			pos.z += moveSpeed;
+			transform.position = transform.position + transform.forward * moveSpeed;
 		if (Input.GetKey(KeyCode.S))
-			pos.z -= moveSpeed;
+			transform.position = transform.position - transform.forward * moveSpeed;
 
-		// Left/Right.
+		// Left/Right
 		if (Input.GetKey(KeyCode.A))
-			pos.x -= moveSpeed;
+			transform.position = transform.position - transform.right * moveSpeed;
 		if (Input.GetKey(KeyCode.D))
-			pos.x += moveSpeed;
+			transform.position = transform.position + transform.right * moveSpeed;
 
 		// Up/Down
 		if (Input.GetKey(KeyCode.Space))
-			pos.y += moveSpeed;
+			transform.position = transform.position + new Vector3(0, moveSpeed, 0);
 		if (Input.GetKey(KeyCode.LeftControl))
-			pos.y -= moveSpeed;
+			transform.position = transform.position - new Vector3(0, moveSpeed, 0);
 
-		if(Input.GetMouseButton(0))
+		// Look
+		if (Input.GetMouseButton(0))
 		{
+			yaw += lookSpeed * Input.GetAxis("Mouse X");
+			pitch -= lookSpeed * Input.GetAxis("Mouse Y");
 
+			transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
 		}
-
-		transform.position = pos;
-	}
-
-	void OnMouseDown()
-	{
-
 	}
 }
